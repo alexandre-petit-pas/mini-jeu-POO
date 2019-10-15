852a2b4ebab74b00a50c6acccabf0dcea3104021
Juliette Petit-Pas <petitpasjuliette@gmail.com>
2019-10-15 15:47:27 +0200

    Mise à jour des fichiers

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Player
   attr_accessor :name, :life_points
 
@@ -7,14 +9,31 @@ class Player
   end
 
   def show_state
-    return "#{name} a #{life_points} points de vie"
+    return "#{name} a #{@life_points} points de vie"
   end
 
-  def gets_damage(damage_number)
-    @life_points -= damage_number
-    if life_points <= 0
+  def gets_damage(shot)
+    @life_points -= shot
+    if @life_points <= 0
       return "#{name} a été tué !"
     end
   end
 
+  def attacks(target)
+    puts "#{@name} attaque #{target.name}"
+    attaque = compute_damage
+    puts "#{@name} lui inflige #{attaque} points de dégât"
+    target.gets_damage(attaque)
+  end
+
+  def compute_damage
+    return rand(1..6)
+  end
+
 end
+
+player1 = Player.new("José")
+player2 = Player.new("Josiane")
+
+
+#binding.pry
